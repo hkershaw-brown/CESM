10d469f3f48c78680f65e9d6022adb45c653fd49
Helen Kershaw <helen_kershaw@brown.edu>
2020-04-27 11:03:20 -0600

    ww3 and cice for wave-ice interact
    Run 1 day on Cheyenne
    Oscar - compile problem.  I think this is the ESMF version.  Cheyenne is now 8.1.0b11, Oscar is 8.0.0
    
    Compiling straight CESM or wave-ice version on Oscar gives
     err:
    
    ERROR:  No results found for variable CAM_DYCORE
    esmFlds.F90(360): #error: #if: syntax error.
    esmFlds.F90(380): #error: #if: syntax error.
    esmFlds.F90(447): #error: #if: syntax error.
    esmFlds.F90(471): #error: #if: syntax error.
    
    Note the cime repository has this problem:
    
    Processing externals description file : ../Externals_cime.cfg
    Processing externals description file : .gitmodules
    Processing submodules description file : .gitmodules
    WARNING:root:WARNING: Ignoring unknown branch property, in /glade/u/home/hkershaw/nuopc/no_ungridded/cesm-helen/cime/src/drivers/nuopc/.gitmodules
    WARNING:root:WARNING: Ignoring unknown branch property, in /glade/u/home/hkershaw/nuopc/no_ungridded/cesm-helen/cime/src/drivers/nuopc/.gitmodules

---
+++ all_status.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+
+echo UFSCOMP $pwd
+
+rootdir=$(pwd)
+
+components=( "."                 \
+             "cime"              \
+             "components/cam"    \
+             "components/cice"   \
+             "components/cism"   \
+             "components/clm"    \
+             "components/mom"    \
+             "components/mosart" \
+             "components/pop"   \
+             "components/ww3") 
+
+for comp in "${components[@]}"
+do
+  cd $comp
+  echo "====="
+  echo " "$comp
+  echo "====="
+  #git status | grep modified
+  mods=$(git status | grep modified)
+  echo -e "\e[31m$mods\e[0m"
+  branchname=$(git rev-parse --abbrev-ref HEAD)
+  remotename=$(git remote -v | grep fetch)
+  echo $branchname '::' $remotename
+  cd $rootdir
+done
+
+cd cime/src/drivers/nuopc
+  echo "====="
+  echo " " cime/src/drivers/nuopc
+  echo "====="
+
+branchname=$(git rev-parse --abbrev-ref HEAD)
+remotename=$(git remote -v | grep fetch)
+mods=$(git status | grep modified)
+echo -e "\e[31m$mods\e[0m"
+echo $branchname '::' $remotename
